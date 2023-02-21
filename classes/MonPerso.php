bed7902f0a109d5555af0aebc4fe10ed547bbf99
kelamJ <malekjulien80@gmail.com>
2023-02-21 11:44:57 +0100

    Update

---
+++ classes/MonPerso.php
@@ -0,0 +1,11 @@
+<?php
+include("/Personnage.class.php");
+
+$p = new Personnage();
+
+$p->getNom("Malek");
+$p->getPrenom("Julien");
+$p->getAge(22);
+$p->getSexe("Homme");
+
+echo ($p);
---
+++ classes/Personnage.class.php
@@ -0,0 +1,34 @@
+<?php
+
+class Personnage
+{
+    private $nom;
+    private $prenom;
+    private $age;
+    private $sexe;
+
+    public function getNom($gNom)
+    {
+        return $this->nom = $gNom;
+    }
+
+    public function getPrenom($gPrenom)
+    {
+        return $this->prenom = $gPrenom;
+    }
+
+    public function getAge($gAge)
+    {
+        return $this->age = $gAge;
+    }
+
+    public function getSexe($gSexe)
+    {
+        return $this->sexe = $gSexe;
+    }
+
+    public function __toString()
+    {
+        return "Nom d'utilisateur : " . $this->nom . "<br>" . "Prénom : " . $this->prenom . "<br>" . "Age :" . $this->age . "<br>" . $this->sexe;
+    }
+}
