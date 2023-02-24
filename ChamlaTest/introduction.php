aae7905d6a37219ae465817031307519e5408a50
kelamJ <malekjulien80@gmail.com>
2023-02-24 11:11:12 +0100

    update

--- ChamlaTest/introduction.php
+++ ChamlaTest/introduction.php
@@ -1,91 +1,88 @@
 <?php
 
-abstract class Humain
-{
-    public $nom;
-    public $prenom;
-    protected $age;
+// abstract class Humain
+// {
+//     public $nom;
+//     public $prenom;
+//     protected $age;
 
-    public function __construct($prenom, $nom, $age)
-    {
-        $this->nom = $nom;
-        $this->prenom = $prenom;
-        $this->setAge($age);
-    }
+//     public function __construct($prenom, $nom, $age)
+//     {
+//         $this->nom = $nom;
+//         $this->prenom = $prenom;
+//         $this->setAge($age);
+//     }
 
-    abstract public function travailler();
+//     abstract public function travailler();
 
-    public function setAge($age)
-    {
-        if(is_int($age) && $age > 1 && $age <= 120){
-            $this->age =$age;
-        } else {
-            throw new Exception("L'âge d'un employé devrait être  un entier entre 1 et 120 !");
-        }
-    }
-    public function getAge($age)
-    {
-        return $this->age;
-    }
-}
-class Employe extends Humain
-{
-    public function travailler()
-    {
-        return "Je suis un employe et je travaille";
-    }
+//     public function setAge($age)
+//     {
+//         if(is_int($age) && $age > 1 && $age <= 120){
+//             $this->age =$age;
+//         } else {
+//             throw new Exception("L'âge d'un employé devrait être  un entier entre 1 et 120 !");
+//         }
+//     }
+//     public function getAge($age)
+//     {
+//         return $this->age;
+//     }
+// }
+// class Employe extends Humain
+// {
+//     public function travailler()
+//     {
+//         return "Je suis un employe et je travaille";
+//     }
 
-    public function presentation() 
-    {
-        var_dump("Salut, je suis $this->prenom $this->nom et j'ai $this->age ans");
-    }
-}
+//     public function presentation() 
+//     {
+//         var_dump("Salut, je suis $this->prenom $this->nom et j'ai $this->age ans");
+//     }
+// }
 
-class Patron extends Employe
-{
-        public $voiture;
+// class Patron extends Employe
+// {
+//         public $voiture;
 
-    public function __construct($prenom, $nom, $age, $voiture)
-    {
-        parent::__construct($prenom, $nom, $age);
+//     public function __construct($prenom, $nom, $age, $voiture)
+//     {
+//         parent::__construct($prenom, $nom, $age);
 
-        $this->voiture = $voiture;
-    }
+//         $this->voiture = $voiture;
+//     }
 
-public function travailler()
-{
-    return "Je suis le patron et je travaille aussi !";
-}
-    public function presentation() 
-    {
-        var_dump("Bonjour, je suis $this->prenom $this->nom et j'ai $this->age ans,et j'ai une voiture !");
-    }
+// public function travailler()
+// {
+//     return "Je suis le patron et je travaille aussi !";
+// }
+//     public function presentation() 
+//     {
+//         var_dump("Bonjour, je suis $this->prenom $this->nom et j'ai $this->age ans,et j'ai une voiture !");
+//     }
 
-    public function rouler ()
-    {
-        var_dump("Bonjour, je roule avec ma $this->voiture");
-    }
-}
+//     public function rouler ()
+//     {
+//         var_dump("Bonjour, je roule avec ma $this->voiture");
+//     }
+// }
 
-$employe1 = new Employe("Malek", "Julien", 22);
-$employe2 = new Employe("Diouf", "Geoffrey", 30);
-$employe1->presentation();
+// $employe1 = new Employe("Malek", "Julien", 22);
+// $employe2 = new Employe("Diouf", "Geoffrey", 30);
+// $employe1->presentation();
 
-$patron = new Patron("Joseph", "Durand" , 50, "Ferrari");
+// $patron = new Patron("Joseph", "Durand" , 50, "Ferrari");
 
-//Impossible
-//$humain = new Humain("Anne", "Durand", 18)
+// $patron->presentation();
+// $patron->rouler();
 
-$patron->presentation();
-$patron->rouler();
-
-class Etudiant extends Humain
-{
-    public function travailler()
-    {
-        return "Je suis étudiant et je révise";
-    }
-}
+// class Etudiant extends Humain
+// {
+//     public function travailler()
+//     {
+//         return "Je suis étudiant et je révise";
+//     }
+// }
 
 // On apelle les focntions
 faireTravailler($employe1);
