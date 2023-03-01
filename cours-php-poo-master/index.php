734a845e7a67a9d8267e275c1ca1dbf6ccd84837
kelamJ <malekjulien80@gmail.com>
2023-03-01 11:06:12 +0100

    Update namespace

--- cours-php-poo-master/index.php
+++ cours-php-poo-master/index.php
@@ -6,19 +6,8 @@
  * On va donc se connecter à la base de données, récupérer les articles du plus récent au plus ancien (SELECT * FROM articles ORDER BY created_at DESC)
  * puis on va boucler dessus pour afficher chacun d'entre eux
  */
-require_once('libraries/database.php');
-require_once('libraries/utils.php');
-require_once('libraries/models/Article.php');
-require_once('libraries/models/User.php');
+require_once('libraries/controllers/Article.php');
 
-$model = new Article();
+$controller = new \Controllers\Article();
+$controller->index();
 
- //* 2. Récupération des articles
-
-$articles = $model->findAll("created_at DESC");
-
-// * 3. Affichage
-
-$pageTitle = "Accueil";
-
-render('articles/index', compact('pageTitle', 'articles'));
